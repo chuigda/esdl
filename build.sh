04bc55208c7f1ce37f3a8ea392de7551a8da4e13
Chuigda <icey@icey.tech>
2023-05-05 13:52:17 +0800

    skeleton

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 tbnf -lang esdl -be python-lark esdl.tbnf
+
+# don't know why @thautwarm uses a really complicated "relative" import.
+# setting up that seems to need much effort, so simply do some dirty dirty sed.
 sed -i 's/.esdl_/esdl_/g' esdl_parser.py
 sed -i 's/.esdl_/esdl_/g' esdl_construct.py
--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+import argparse
+from pathlib import Path
+
 from esdl_parser import parser
 
+
+arg_parser = argparse.ArgumentParser(
+    prog='esdl',
+    description='ESDL Data Definition Language Compiler',
+    epilog='File issues or star and thank author at https://github.com/chuigda/esdl'
+)
+arg_parser.add_argument('-i', dest='input', help='input file', required=True, type=Path)
+arg_parser.add_argument('-g', dest='generator', help='select code generator', choices=[
+    'rs',
+    'js',
+    'py',
+    'cxx'
+], default='rs')
+
+
 if __name__ == '__main__':
-    # read all from 'rbuf.esdl' file
-    with open('rbuf.esdl', 'r') as f:
-        rbuf = f.read()
-        # parse
-        r = parser.parse(rbuf)
-        # print result
-        print(r)
+    args = arg_parser.parse_args()
+    with open(args.input, 'r') as input_file:
+        parsed_tree = parser.parse(input_file.read())
+        print(parsed_tree)
